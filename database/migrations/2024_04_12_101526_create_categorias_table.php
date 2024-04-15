4bda1885575335785f85e95571808d9e4c90f6e3
Otto Szarata <szystems@hotmail.com>
2024-04-15 10:44:09 -0600

    migration categoria arreglo

--- database/migrations/2024_04_12_101526_create_categorias_table.php
+++ database/migrations/2024_04_12_101526_create_categorias_table.php
@@ -16,11 +16,7 @@ class CreateCategoriasTable extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-<<<<<<< HEAD
-            $table->text('descripcion')->nullable();
-=======
             $table->text('descripcion')->nullable()->default('Sin descripción');
->>>>>>> 1c3e5824fb89ce0b9852ad1a120402dd14f97a45
             $table->tinyInteger('estado')->default(true);
             $table->timestamps();
         });
