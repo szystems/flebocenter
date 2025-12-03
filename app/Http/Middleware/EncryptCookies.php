5e2a5b25a3c6b7744dc6ba0f2a20576e714df43c
szystems <szystems@hotmail.com>
2025-12-02 20:10:30 -0800

    Fix: Excluir flebocenter_session de encriptación
    PROBLEMA: Cookie simple funciona, Laravel session NO
    
    CAUSA: EncryptCookies middleware bloqueando cookie de sesión
    
    SOLUCIÓN: Excluir flebocenter_session de encriptación

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -12,6 +12,6 @@ class EncryptCookies extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'flebocenter_session', // Excluir cookie de sesión de encriptación
     ];
 }
